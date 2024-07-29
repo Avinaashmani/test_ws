469d38c9c7e76c6058e75ae75c92a8159ba7b648
avinaash <avinaash.mani@gmail.com>
2024-07-29 12:08:04 +0530

    gearfork bot robot

---
+++ test_ws/build/gearfork_bot_bringup/CMakeFiles/gearfork_bot_bringup_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gearfork_bot_bringup_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/gearfork_bot_bringup_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
